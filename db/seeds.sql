1e3d180bb07986bf75499ba9445f44a4d90fa83c
Arezoo Tajerzadeh <Arezoo Tajerzadeh>
2017-05-06 14:10:44 -0700

    upload files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO pets (burger_name, devoured, date) VALUES ('Buffalo', 'true', NOW());
+INSERT INTO pets (burger_name, devoured, date) VALUES ('Cheeseburger', 'false', NOW());
+INSERT INTO pets (burger_name, devoured, date) VALUES ('Veggie ', 'true', '2017-05-01 18:19:03');
+
---
+++ server.js
@@ -0,0 +1,7 @@
+//Require 
+//express
+//method-override
+//body-parser
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("body-parser");
